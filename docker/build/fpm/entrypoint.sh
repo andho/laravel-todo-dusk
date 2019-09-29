e793b341be0947c77cb83dfc8df6a5a81fe830e0
Amjad Mohamed <andhos@gmail.com>
2019-09-29 16:35:40 +0500

    Using original php-fpm entrypoint and added artisan commands

--- docker/build/fpm/entrypoint.sh
+++ docker/build/fpm/entrypoint.sh
@@ -1,7 +1,13 @@
-#!/bin/bash
+#!/bin/sh
 set -e
 
+cd /var/www
 ./artisan cache:clear
 ./artisan config:cache
 
-exec /usr/local/bin/docker-php-entrypoint "$@"
+# first arg is `-f` or `--some-option`
+if [ "${1#-}" != "$1" ]; then
+	set -- php-fpm "$@"
+fi
+
+exec "$@"
